9c39b4c6aa122b872d4766ed4e365f1133130c3f
lee kinanga <lee254kinanga@gmail.com>
2024-05-22 01:39:40 +0300

    Top nav UI made

--- main/js/admin-dash.js
+++ main/js/admin-dash.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('keydown', function(event) {
         // console.log('Key pressed:', event.key);
@@ -32,3 +33,43 @@ document.addEventListener('DOMContentLoaded', function() {
         stylesheet.setAttribute('href', cssFile);
     }
 });
+
+document.getElementById('collapse').addEventListener('click', function() {
+   
+    CollapseRestore();
+
+});
+document.getElementById('logo-left').addEventListener('click', function() {
+   
+    CollapseRestore();
+
+});
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.shiftKey) {
+      if (event.key === '<') {
+        Collapse(); 
+      } else if (event.key === '>') {
+        Restore(); 
+      }
+    }
+  });
+
+function Collapse(){
+    document.getElementById('leftnav').style.width = '6%';
+    document.getElementById('main').style.width = '94%';
+}
+
+function Restore(){
+    document.getElementById('leftnav').style.width = '14%';
+    document.getElementById('main').style.width = '86%';
+}
+
+function CollapseRestore(){
+    var widthstatus = document.getElementById('leftnav').style.width;
+
+   if (widthstatus === '14%' || widthstatus === 'auto' || widthstatus === '') {
+    Collapse();
+  } else {
+    Restore();
+  }
+}
